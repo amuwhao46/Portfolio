403a34102e0bdbe282f455c181234c6d56d21db4
Oke Amuwha <amuwhao46@gmail.com>
2021-04-09 17:34:01 -0400

    Reformatted linkedIn Embed
    Used a different linked in badge to express how my webpage is simple and easy to follow. Also changed the title to make it easier to follow

--- portfolio.php
+++ portfolio.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
-      <title>Portfolio</title>
+      <title>Portfolio - Home</title>
    </head>
    <body class="bg-gray-700 min-h-screen flex flex-col min-w-screen">
       <header class="bg-red-600 p-4 shadow inset-y-0 top-0">
@@ -24,7 +24,8 @@
             <!--<img src="image007_lrg.jpg" alt="ship">-->
             <!--Linkedin badge-->
             <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
-            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+            <br>
             <!--Github badge-->
             <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
             <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
