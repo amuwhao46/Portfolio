b93688d4f3db03f6742b48db0b7abd8970d71db6
Oke Amuwha <amuwhao46@gmail.com>
2021-04-09 17:24:08 -0400

    Added more text and fixed formatting
    Added more text to describe my experience and finally fixed the formatting for linkedIn and github embedded links

--- portfolio.php
+++ portfolio.php
@@ -15,19 +15,19 @@
         <main class="flex p-10">
         <div class="flex flex-col">
           <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
-          <p>Testing next set of lines</p>
-          <p>Testing next set of lines</p>
+          <p>My first experience with anything computer related was with hosting my own server to play minecraft with my freinds at 12</p>
+          <p>Had to teach myself about portfowarding and connecting multiple servers to one server and making them work with each other</p>
           <p>Testing next set of lines</p>
           <p>Testing next set of lines</p>
         </div>
-        <div class="flex flex-col flex-justify-end w-full">
+        <div class="flex flex-col item-center justify-end">
             <!--<img src="image007_lrg.jpg" alt="ship">-->
-              <!--Linkedin badge-->
-              <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
-              <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-              <!--Github badge-->
-              <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
-              <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+            <!--Linkedin badge-->
+            <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+            <!--Github badge-->
+            <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
+            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
         </div>
         </main>
       </div>
