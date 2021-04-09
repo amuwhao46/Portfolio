212038098bdee39d76b66223e820b2e4e06a2395
Oke Amuwha <amuwhao46@gmail.com>
2021-04-09 15:54:15 -0400

    Added Github embed link
    Added embedded link badge to my github repositories

--- portfolio.php
+++ portfolio.php
@@ -1,20 +1,26 @@
 <!doctype html>
 <html>
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
-  <title>Portfolio</title>
-</head>
-<body class="bg-red-600">
-  <header class="bg-white p-4 shadow inset-y-0 top-0"><div class="text-center text-lg mx-auto">Oke's Journey</div></header>
-  <div class="text-white">
-    <main>writing stuff random stuff for the main.</main>
-  </div>
-  <footer class="bg-gray-100 p-4 shadow inset-y-0 bottom-0">
-  <p>Oke-Oghene Amuwha</p>
-  <p>Custom Portfolio</p>
-  </footer>
-</body>
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+      <title>Portfolio</title>
+   </head>
+   <body class="bg-white min-h-screen flex flex-col min-w-screen">
+      <header class="bg-red-600 p-4 shadow inset-y-0 top-0">
+         <div class="text-center text-lg mx-auto">Oke's Journey</div>
+      </header>
+      <div class="text-black shadow">
+         <main class="flex p-10">
+            <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
+            <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
+            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+         </main>
+      </div>
+      <footer class="bg-gray-100 p-4 shadow bottom-0 absolute w-full">
+         <p>Oke-Oghene Amuwha</p>
+         <p>Custom Portfolio</p>
+      </footer>
+   </body>
 </html>
