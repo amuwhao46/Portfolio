81ffb64cf1c48df2d6ec88dfdb464089d456e948
Oke Amuwha <amuwhao46@gmail.com>
2021-04-09 15:02:32 -0400

    Setting up layout of website

---
+++ portfolio.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+  <title>Portfolio</title>
+</head>
+<body class="bg-red-600">
+  <header class="text-center text-white text-lg mx-auto">Oke's Journey</header>
+  <main></main>
+  <footer></footer>
+</body>
+</html>
