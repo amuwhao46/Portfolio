e31c3a93085927942f697fd51baa10eb051f6ccc
Oke Amuwha <amuwhao46@gmail.com>
2021-04-23 15:49:19 -0400

    Formatted card with flex function in tailwind
    Used tailwindcss to create a skeleton of the custom card I am making. Also cleaned up code to reduce redundancy. Added a second text to test the card format

--- portfolio.php
+++ portfolio.php
@@ -11,20 +11,20 @@
       <header class="bg-gray-300 p-4 shadow inset-y-0 top-0">
          <div class="text-center text-lg mx-auto">Oke's Journey</div>
       </header>
-      <div class="text-gray-200">
-        <main class="flex p-10 justify-center">
-        <div class="flex flex-col p-5">
-        <div class="bg-white rounded p-10">
+        <main class="flex justify-center text-gray-800">
+        <div class="flex flex-col bg-white rounded-lg p-10">
+        <div>
           <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
           <p>My first experience with anything computer related was with hosting my own server to play minecraft with my freinds at 12</p>
           <p>Had to teach myself about portfowarding and connecting multiple servers to one server and making them work with each other</p>
           <p>Testing next set of lines</p>
           </div>
+          <div class="flex items-center space-x-10 justify-center mt-10">
           <!--Card Component-->
-          <div class="bg-white rounded p-10">
+          <div class="shadow rounded-lg p-5">
           <!--name-->
             <div>
-            <p>name</p>
+            <p>Oke-Oghene Amuwha</p>
             </div>
             <!--Username-->
             <div>
@@ -34,13 +34,17 @@
             <div>
             <p>name.com</p>
             </div>
+            </div>
+            <!--End card-->
+            <div>
+            lol
+            </div>
           </div>
-          <p>Testing next set of lines</p>
-          <div class="flex items-center space-x-2">
+         <!-- <div class="flex items-center space-x-2">
           <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
             <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
             <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
-            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   
+            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   -->
             </div>                         
         </div>
         <div class="flex flex-col item-center justify-end">
@@ -54,7 +58,6 @@
             <!--Github badge-->
         </div>
         </main>
-      </div>
       <footer class="bg-gray-800 p-4 bottom-0 absolute w-full text-gray-200">
          <p>Oke-Oghene Amuwha</p>
          <p>Custom Portfolio</p>
