101f5cfe75c8e288ebf12b58dc998888fce6f240
Oke Amuwha <amuwhao46@gmail.com>
2021-04-23 15:21:40 -0400

    Started working on custom cards
    After reformatting the cards from LinkedIn and Github I decided to start making a custom one to show case my profile and not mess up the formatting

--- portfolio.php
+++ portfolio.php
@@ -8,31 +8,54 @@
       <title>Portfolio - Home</title>
    </head>
    <body class="bg-gray-700 min-h-screen flex flex-col min-w-screen">
-      <header class="bg-red-600 p-4 shadow inset-y-0 top-0">
+      <header class="bg-gray-300 p-4 shadow inset-y-0 top-0">
          <div class="text-center text-lg mx-auto">Oke's Journey</div>
       </header>
-      <div class="text-black">
-        <main class="flex p-10">
-        <div class="flex flex-col">
+      <div class="text-gray-200">
+        <main class="flex p-10 justify-center">
+        <div class="flex flex-col p-5">
+        <div class="bg-white rounded p-10">
           <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
           <p>My first experience with anything computer related was with hosting my own server to play minecraft with my freinds at 12</p>
           <p>Had to teach myself about portfowarding and connecting multiple servers to one server and making them work with each other</p>
           <p>Testing next set of lines</p>
+          </div>
+          <!--Card Component-->
+          <div class="bg-white rounded p-10">
+          <!--name-->
+            <div>
+            <p>name</p>
+            </div>
+            <!--Username-->
+            <div>
+            <p>@name</p>
+            </div>
+            <!--Hyperlink-->
+            <div>
+            <p>name.com</p>
+            </div>
+          </div>
           <p>Testing next set of lines</p>
+          <div class="flex items-center space-x-2">
+          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+            <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
+            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   
+            </div>                         
         </div>
         <div class="flex flex-col item-center justify-end">
+        <br>
             <!--<img src="image007_lrg.jpg" alt="ship">-->
-            <!--Linkedin badge-->
-            <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+
+            <!--Linkedin badge
+            <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
             <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-            <br>
+            <br> -->
             <!--Github badge-->
-            <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
-            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
         </div>
         </main>
       </div>
-      <footer class="bg-gray-800 p-4 bottom-0 absolute w-full">
+      <footer class="bg-gray-800 p-4 bottom-0 absolute w-full text-gray-200">
          <p>Oke-Oghene Amuwha</p>
          <p>Custom Portfolio</p>
       </footer>
