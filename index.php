4196a36ac0d4569a45fa809779a6c1a6de97e5e5
Oke Amuwha <amuwhao46@gmail.com>
2021-05-17 21:52:10 -0400

    Added global formatting rules for portfolio
    Started set up for the global formatting rules in the OOD of my portfolio

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <html>
+
 <head>
 <title>Portfolio</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gray-200 min-h-screen flex flex-col min-w-screen">
 </body>
+
 </html>
