5ad8d983af53bbe26a8ae3b8dd19c68903f48eee
Oke Amuwha <amuwhao46@gmail.com>
2021-06-25 12:18:41 -0400

    Edited formatting
    Used an external application to edit the formatting of my code for readability

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <html>
-
-<head>
-<title>Portfolio</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
-<body class="bg-gray-200 min-h-screen flex flex-col min-w-screen">
-<!--header-->
-<header class="">
-<?php include_once('sections/header.php');?>
-</header>
-<!--main-->
-<main class="flex justify-center text-gray-800">
-<?php include_once('sections/main.php');?>
-</main>
-<!--footer-->
-<footer>
-<?php include_once('sections/footer.php');?>
-</footer>
-</body>
-
+  <head>
+    <title>Portfolio</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  </head>
+  <body class="bg-gray-200 min-h-screen flex flex-col min-w-screen">
+    <!--header-->
+    <header class="">
+      <?php include_once('sections/header.php');?>
+    </header>
+    <!--main-->
+    <main class="flex justify-center text-gray-800">
+      <?php include_once('sections/main.php');?>
+    </main>
+    <!--footer-->
+    <footer>
+      <?php include_once('sections/footer.php');?>
+    </footer>
+  </body>
 </html>
--- sections/main.php
+++ sections/main.php
@@ -1,89 +1,84 @@
 <main class="flex justify-center text-gray-800 shadow-lg">
   <div class="flex flex-col bg-gray-100 rounded-lg p-10">
     <div class="p-4 rounded bg-blue-500 text-white word-break">
-    <div class="font-medium tracking-widest"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
-    </svg>
-    </div>
-    <br>
+      <div class="font-medium tracking-widest">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+        </svg>
+      </div>
+      <br>
       <span class="tracking-wide">
-      <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
-      <p>My first experience with anything computer related was with hosting my own server to play minecraft with my freinds at 12</p>
-      <p>Had to teach myself about portfowarding and connecting multiple servers to one server and making them work with each other</p>
-      <p>Testing next set of lines</p>
+        <p>September 2000, the year I was born. On that wonderful day I was brought into the world despite not having a choice.</p>
+        <p>My first experience with anything computer related was with hosting my own server to play minecraft with my freinds at 12</p>
+        <p>Had to teach myself about portfowarding and connecting multiple servers to one server and making them work with each other</p>
+        <p>Testing next set of lines</p>
       </span>
     </div>
-
-  <!--Enclosed in the whole card-->
-  <div class="flex items-center space-x-10 justify-center mt-10">
-
-  <!--Card Component-->
-  <div>
-  <div class="bg-blue-500 text-white p-5 rounded-lg shadow flex flex-col"> 
-            <!--Logo-->
-            <div>
-    <p>LinkedIn</p>
-    </div>
-  </div>
-  <div class="shadow rounded-lg p-5">
-  <!--name-->
-    <div>
-    <p>Oke-Oghene Amuwha</p>
-    </div>
-    <!--Username-->
-    <div>
-    <p>@name</p>
-    </div>
-    <!--Hyperlink-->
-    <div>
-    <a href="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/">Oke's linkedIn</a>
-    </div>
-    </div>
-    </div>
-    <!--End card-->
-
-
-  <!--Card Component-->
-  <div>
-  <div class="bg-blue-500 text-white p-5 rounded-lg shadow flex flex-col"> 
-            <!--Logo-->
-            <div>
-    <p>LinkedIn</p>
-    </div>
-  </div>
-  <div class="shadow rounded-lg p-5">
-  <!--name-->
-    <div>
-    <p>Oke-Oghene Amuwha</p>
-    </div>
-    <!--Username-->
-    <div>
-    <p>@name</p>
-    </div>
-    <!--Hyperlink-->
-    <div>
-    <a href="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/">Link to LinkedIn</a>
-    </div>
-    </div>
-    </div>
-    <!--End card-->
-  </div>
-
-         <!-- <div class="flex items-center space-x-2">
-          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
-            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-            <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
-            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   -->
-            </div>                         
+    <!--Enclosed in the whole card-->
+    <div class="flex items-center space-x-10 justify-center mt-10">
+      <!--Card Component-->
+      <div>
+        <div class="bg-blue-500 text-white p-5 rounded-lg shadow flex flex-col">
+          <!--Logo-->
+          <div>
+            <p>LinkedIn</p>
+          </div>
         </div>
-        <div class="flex flex-col item-center justify-end">
-        <br>
-            <!--<img src="image007_lrg.jpg" alt="ship">-->
-
-            <!--Linkedin badge
-            <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
-            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-            <br> -->
-            <!--Github badge-->
+        <div class="shadow rounded-lg p-5">
+          <!--name-->
+          <div>
+            <p>Oke-Oghene Amuwha</p>
+          </div>
+          <!--Username-->
+          <div>
+            <p>@name</p>
+          </div>
+          <!--Hyperlink-->
+          <div>
+            <a href="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/">Oke's linkedIn</a>
+          </div>
         </div>
-        </main>
+      </div>
+      <!--End card-->
+      <!--Card Component-->
+      <div>
+        <div class="bg-blue-500 text-white p-5 rounded-lg shadow flex flex-col">
+          <!--Logo-->
+          <div>
+            <p>LinkedIn</p>
+          </div>
+        </div>
+        <div class="shadow rounded-lg p-5">
+          <!--name-->
+          <div>
+            <p>Oke-Oghene Amuwha</p>
+          </div>
+          <!--Username-->
+          <div>
+            <p>@name</p>
+          </div>
+          <!--Hyperlink-->
+          <div>
+            <a href="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/">Link to LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <!--End card-->
+    </div>
+    <!-- <div class="flex items-center space-x-2">
+      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
+        <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+        <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
+        <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   -->
+  </div>
+  </div>
+  <div class="flex flex-col item-center justify-end">
+    <br>
+    <!--<img src="image007_lrg.jpg" alt="ship">-->
+    <!--Linkedin badge
+      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
+      <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
+      <br> -->
+    <!--Github badge-->
+  </div>
+</main>
