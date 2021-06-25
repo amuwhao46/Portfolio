af2bee4fde91eae696e24d86138067864b07a3e9
Oke Amuwha <amuwhao46@gmail.com>
2021-06-25 12:03:32 -0400

    Coupled files together
    Figured out how to make the files show different sections of the portfolio

--- index.php
+++ index.php
@@ -7,11 +7,17 @@
 
 <body class="bg-gray-200 min-h-screen flex flex-col min-w-screen">
 <!--header-->
-
+<header class="">
+<?php include_once('sections/header.php');?>
+</header>
 <!--main-->
-
+<main class="flex justify-center text-gray-800">
+<?php include_once('sections/main.php');?>
+</main>
 <!--footer-->
-
+<footer>
+<?php include_once('sections/footer.php');?>
+</footer>
 </body>
 
 </html>
