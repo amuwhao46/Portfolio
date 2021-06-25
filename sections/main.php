3ce7b2c710b6c0763933a06e819ff4e44c5a1a35
Oke Amuwha <amuwhao46@gmail.com>
2021-06-25 13:20:43 -0400

    Cleaned up code
    Cleaned up code for easier readability and added another card for my github socials

--- sections/main.php
+++ sections/main.php
@@ -45,7 +45,7 @@
         <div class="bg-blue-500 text-white p-5 rounded-lg shadow flex flex-col">
           <!--Logo-->
           <div>
-            <p>LinkedIn</p>
+            <p>Github</p>
           </div>
         </div>
         <div class="shadow rounded-lg p-5">
@@ -55,30 +55,15 @@
           </div>
           <!--Username-->
           <div>
-            <p>@name</p>
+            <p>@amuwhao46</p>
           </div>
           <!--Hyperlink-->
           <div>
-            <a href="https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203/">Link to LinkedIn</a>
+            <a href="https://github.com/amuwhao46">Link to Github</a>
           </div>
         </div>
       </div>
-      <!--End card-->
+    <!--End card-->
     </div>
-    <!-- <div class="flex items-center space-x-2">
-      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
-        <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-        <div class="github-card" data-github="amuwhao46" data-width="400" data-height="" data-theme="default"></div>
-        <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>   -->
-  </div>
-  </div>
-  <div class="flex flex-col item-center justify-end">
-    <br>
-    <!--<img src="image007_lrg.jpg" alt="ship">-->
-    <!--Linkedin badge
-      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> 
-      <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="oke-oghene-amuwha-1b1489203"><a class="LI-simple-link" href='https://www.linkedin.com/in/oke-oghene-amuwha-1b1489203?trk=profile-badge'>Oke-Oghene Amuwha</a></div>
-      <br> -->
-    <!--Github badge-->
   </div>
 </main>
